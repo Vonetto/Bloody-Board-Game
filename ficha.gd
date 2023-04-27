cd22479b393dfa73777633676814371ef21f65b2
Vonetto <juan.onetto@ug.uchile.cl>
2023-04-26 22:47:33 -0400

    added black pieces mvnt

--- ficha.gd
+++ ficha.gd
@@ -79,25 +79,32 @@ func move(cas,pos2, mapa, selector):
 				valid= false
 				print("Invalid Movement")	
 			
-	
 	elif (self.ficha.id == "P" and  self.ficha.team == "Black"):
+		var indice_1=buscar_llave_por_valor(mapa,cas)
+		var indice_2=buscar_llave_por_valor(mapa,pos2)
+		
+		if cas.x != pos2.x:
+			valid=false
+			print("Invalid Movement")
+			
 		if inicio == true: #First move
-			if pos2.y>=-336 and pos2.x == cas.x:
-				self.position = Vector2(cas.x-48, pos2.y+50)
-				inicio=false
+			if indice_1>=49:
+				if abs(indice_2-indice_1 )==16 or abs(indice_2-indice_1 )==8 :
+					self.position = Vector2(cas.x-48, pos2.y+50)
+					inicio=false
 				
-			else:
-				valid = false
-				print("Invalid Movement")
+				
+				else:
+					valid = false
+					print("Invalid Movement")
 		else:
-			if pos2.x == cas.x : 
+			if abs(indice_2-indice_1 )==8 :
 				self.position = Vector2(pos2.x-48, pos2.y+50)
 		
 			else:
-				valid= false
+				valid= false 
 				print("Invalid Movement")	
 		
-			
 	elif (self.ficha.id == "B" and  self.ficha.team == "White"):
 		var indice_1=buscar_llave_por_valor(mapa,cas)
 		var indice_2=buscar_llave_por_valor(mapa,pos2)
@@ -107,10 +114,22 @@ func move(cas,pos2, mapa, selector):
 		
 		else:
 			valid = false
+			print("Invalid Movement")	
+		
+	elif (self.ficha.id == "B" and  self.ficha.team == "Black"):
+		var indice_1=buscar_llave_por_valor(mapa,cas)
+		var indice_2=buscar_llave_por_valor(mapa,pos2)
+		
+		if (abs(indice_2 - indice_1)%7==0) or (abs(indice_2 - indice_1)%9==0):
+			self.position = Vector2(pos2.x-48, pos2.y+50)
+		
+		else:
+			valid = false
 			print("Invalid Movement")
 			
 	
 	elif (self.ficha.id == "N" and  self.ficha.team == "White"):	
+		
 		var poss_movements = [
 			Vector2(cas.x-2*96, cas.y-1*96), # Movimiento hacia arriba y a la izquierda
 			Vector2(cas.x-1*96, cas.y-2*96), # Movimiento hacia la izquierda y hacia arriba
@@ -130,6 +149,27 @@ func move(cas,pos2, mapa, selector):
 			valid = false
 			print("Invalid Movement")
 
+	elif (self.ficha.id == "N" and  self.ficha.team == "Black"):	
+		
+		var poss_movements = [
+			Vector2(cas.x-2*96, cas.y-1*96), # Movimiento hacia arriba y a la izquierda
+			Vector2(cas.x-1*96, cas.y-2*96), # Movimiento hacia la izquierda y hacia arriba
+			Vector2(cas.x+1*96, cas.y-2*96),  # Movimiento hacia la izquierda y hacia abajo
+			Vector2(cas.x+2*96, cas.y-1*96),  # Movimiento hacia abajo y a la izquierda
+			Vector2(cas.x-2*96, cas.y+1*96),  # Movimiento hacia arriba y a la derecha
+			Vector2(cas.x-1*96, cas.y+2*96),  # Movimiento hacia la derecha y hacia arriba
+			Vector2(cas.x+1*96, cas.y+2*96),   # Movimiento hacia la derecha y hacia abajo
+			Vector2(cas.x+2*96, cas.y+1*96)   	
+		]
+		
+		if pos2 in poss_movements:
+			#print(pos2)
+			self.position = Vector2(pos2.x-48, pos2.y+50)
+			
+		else :
+			valid = false
+			print("Invalid Movement")
+					
 	elif (self.ficha.id == "R" and  self.ficha.team == "White"):
 		
 		
@@ -137,6 +177,18 @@ func move(cas,pos2, mapa, selector):
 		var indice_2=buscar_llave_por_valor(mapa,pos2)
 		
 		if ((indice_2 - indice_1)%8==0) or ((indice_2 - indice_1)<=7):
+			self.position = Vector2(pos2.x-48, pos2.y+50)
+		
+		else:
+			valid = false
+			print("Invalid Movement")
+			
+	elif (self.ficha.id == "R" and  self.ficha.team == "Black"):
+		
+		var indice_1=buscar_llave_por_valor(mapa,cas)
+		var indice_2=buscar_llave_por_valor(mapa,pos2)
+		
+		if (abs(indice_2 - indice_1)%8==0) or (abs(indice_2 - indice_1)<=7):
 			self.position = Vector2(pos2.x-48, pos2.y+50)
 		
 		else:
@@ -155,7 +207,20 @@ func move(cas,pos2, mapa, selector):
 			valid = false
 			print("Invalid Movement")
 	
+	elif (self.ficha.id == "Q" and  self.ficha.team == "Black"):
+		
+		var indice_1=buscar_llave_por_valor(mapa,cas)
+		var indice_2=buscar_llave_por_valor(mapa,pos2)
+		
+		if (abs(indice_2 - indice_1)%8==0) or (abs(indice_2 - indice_1)<=7) or (abs(indice_2 - indice_1)%7==0) or (abs(indice_2 - indice_1)%9==0):
+			self.position = Vector2(pos2.x-48, pos2.y+50)
+		
+		else:
+			valid = false
+			print("Invalid Movement")
+			
 	elif (self.ficha.id == "K" and  self.ficha.team == "White"):
+		
 		var indice_1=buscar_llave_por_valor(mapa,cas)
 		var indice_2=buscar_llave_por_valor(mapa,pos2)
 		
@@ -165,7 +230,18 @@ func move(cas,pos2, mapa, selector):
 		else:
 			valid = false
 			print("Invalid Movement")
-			
+	
+	elif (self.ficha.id == "K" and  self.ficha.team == "Black"):
+		var indice_1=buscar_llave_por_valor(mapa,cas)
+		var indice_2=buscar_llave_por_valor(mapa,pos2)
+		
+		if (abs(indice_2 - indice_1) ==1) or (abs(indice_2 - indice_1)==7) or (abs(indice_2 - indice_1)) ==9 or  (abs(indice_2 - indice_1)==8):
+			self.position = Vector2(pos2.x-48, pos2.y+50)
+		
+		else:
+			valid = false
+			print("Invalid Movement")
+	
 	if 	valid == false:
 		selector.modulate = Color(1,0,0)	
 	else:
