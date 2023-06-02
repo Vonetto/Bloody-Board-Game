6ca8048d1bdbce29071c8863c2c8f9e73a316b6a
Francisco Faundez <panchooryo@gmail.com>
2023-06-02 11:44:49 -0400

    starting the red bishops attack

--- Fight_Pieces/Red_Pieces/Red_Bishop/red_bishop.gd
+++ Fight_Pieces/Red_Pieces/Red_Bishop/red_bishop.gd
@@ -7,7 +7,9 @@ const JUMP_VELOCITY = -1000.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*3
 @onready var pivot = $pivot
-
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -32,3 +34,11 @@ func _physics_process(delta):
 		pivot.scale.x = -1
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("Attack"):
+		_attack()
+	else:
+		playback.travel("")
+		
+func _attack():
+	playback.call_deferred("travel", "Attack")
