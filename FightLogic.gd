b9ea79783870a8ac7bfcebe1d2c1750f14253074
Sebastián Jana <you@example.com>
2023-07-01 17:43:15 -0400

    se instancian correctamente los personajes en fight

--- FightLogic.gd
+++ FightLogic.gd
@@ -5,7 +5,21 @@ var current_time: float = 0.0
 var print_timer: Timer
 @onready var timeText = get_node("timeText")
 
-@onready var instancedKnight = preload("res://Fight_Pieces/Blue_Pieces/Blue_Knight/blue_knight.tscn")
+#blue pieces
+@onready var instancedWPawn = preload("res://Fight_Pieces/Blue_Pieces/Blue_Pawn/blue_pawn.tscn")
+@onready var instancedWRook = preload("res://Fight_Pieces/Blue_Pieces/Blue_Rook/blue_rock.tscn")
+@onready var instancedWKnight = preload("res://Fight_Pieces/Blue_Pieces/Blue_Knight/blue_knight.tscn")
+@onready var instancedWBishop = preload("res://Fight_Pieces/Blue_Pieces/Blue_Bishop/blue_bishop.tscn")
+@onready var instancedWQueen = preload("res://Fight_Pieces/Blue_Pieces/Blue_Queen/blue_queen.tscn")
+@onready var instancedWKing = preload("res://Fight_Pieces/Blue_Pieces/Blue_King/blue_king.tscn")
+
+#red pieces
+@onready var instancedBPawn = preload("res://Fight_Pieces/Red_Pieces/Red_Pawn/red_pawn.tscn")
+@onready var instancedBRook = preload("res://Fight_Pieces/Red_Pieces/Red_Rook/red_rook.tscn")
+@onready var instancedBKnight = preload("res://Fight_Pieces/Red_Pieces/Red_Knight/red_knight.tscn")
+@onready var instancedBBishop = preload("res://Fight_Pieces/Red_Pieces/Red_Bishop/red_bishop.tscn")
+@onready var instancedBQueen = preload("res://Fight_Pieces/Red_Pieces/Red_Queen/red_queen.tscn")
+@onready var instancedBKing = preload("res://Fight_Pieces/Red_Pieces/Red_King/red_king.tscn")
 
 func _ready():
 	print_timer = Timer.new()
@@ -16,12 +30,49 @@ func _ready():
 
 	startTimer()
 
+func _instance_ficha(ficha, pos):
+	var Player = ficha.instantiate()
+	add_child(Player)
+	Player.position = pos
+
 func _on_fight_started(ficha1, ficha2):
-	var J1 = instancedKnight.instantiate()
-	get_tree().get_root().add_child(J1)
-	J1.position = Vector2(530, 524)
-	print(ficha1)
-	print(ficha2)
+	var J1 = ficha1
+	var J2 = ficha2
+	var Player = null
+	var pos = null
+	for ficha in [J1, J2]:
+		var team = ficha.ficha["team"]
+		var id = ficha.ficha["id"]
+		#equipos
+		if team == "White":
+			pos = Vector2(530, 524)
+			if id == "P":
+				Player = instancedWPawn
+			elif id == "R":
+				Player = instancedWRook
+			elif id == "N":
+				Player = instancedWKnight
+			elif id == "B":
+				Player = instancedWBishop
+			elif id == "Q":
+				Player = instancedWQueen
+			elif id == "K":
+				Player = instancedWKing
+		elif team == "Black":
+			pos = Vector2(530, 524)
+			if id == "P":
+				Player = instancedWPawn
+			elif id == "R":
+				Player = instancedWRook
+			elif id == "N":
+				Player = instancedWKnight
+			elif id == "B":
+				Player = instancedWBishop
+			elif id == "Q":
+				Player = instancedWQueen
+			elif id == "K":
+				Player = instancedWKing
+		_instance_ficha(Player, pos)
 
 func startTimer():
 	current_time = timer_duration
