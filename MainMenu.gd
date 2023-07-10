1d99e46c8479c39fa28a0a2ac8955a89e1cc6adb
FranciscoFaundez <panchooryo@gmail.com>
2023-07-10 19:33:23 -0400

    Added a main menu and a pause menu

---
+++ MainMenu.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+@onready var play = %Play
+@onready var exit = %Exit
+@onready var credits = %Credits
+
+func _ready():
+	play.pressed.connect(_on_play_pressed)
+	exit.pressed.connect(_on_exit_pressed)
+	credits.pressed.connect(_on_credits_pressed)
+	
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://credits.tscn")
