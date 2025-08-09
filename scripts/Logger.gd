b7eb4ce6e37e51f0a7bd98bfd829b5e4fa529bd3
Vonetto <juan.onetto@ug.uchile.cl>
2025-08-09 01:23:25 -0400

    chore(logger): configure Logger as autoload and remove class_name to avoid singleton hiding

--- scripts/Logger.gd
+++ scripts/Logger.gd
@@ -1,24 +1,20 @@
 extends Node
 
-class_name Logger
-
 @export var enabled: bool = true
 @export var level: int = 0 # 0=debug, 1=info, 2=warn, 3=error
 
 static func d(msg: String) -> void:
-    if ProjectSettings.get_setting("application/run/low_processor_mode"):
-        return
-    print("[DEBUG] ", msg)
+	if ProjectSettings.get_setting("application/run/low_processor_mode"):
+		return
+	print("[DEBUG] ", msg)
 
 static func i(msg: String) -> void:
-    if ProjectSettings.get_setting("application/run/low_processor_mode"):
-        return
-    print("[INFO] ", msg)
+	if ProjectSettings.get_setting("application/run/low_processor_mode"):
+		return
+	print("[INFO] ", msg)
 
 static func w(msg: String) -> void:
-    print("[WARN] ", msg)
+	print("[WARN] ", msg)
 
 static func e(msg: String) -> void:
-    push_error(msg)
-
-
+	push_error(msg)
